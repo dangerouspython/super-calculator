2c5f2d4eeb574d7a2c3a4441e9184050c503dd30
Hrudaya Ranjan Sahoo <hrudayranjansahoo0208@gmail.com>
2015-01-22 20:57:37 +0530

    Finally got the buttons to work by making them use a single function.

--- calculator.py
+++ calculator.py
@@ -26,19 +26,19 @@ class ControlPanel(Frame):
         self.disp = disp_area
         self.num1 = num1
         self.num2 = num2
+        
         Button(self, text="Reset", command=self.reset,
-               bg="#A00000", fg="#FFFFFF", relief="raised",
-               font="Helvetica").pack()
-        Button(self, text="Add!", command=self.calc,
+               bg="#A00000", fg="#FFFFFF", font="Helvetica").pack()
+        Button(self, text="Add!", command=lambda m="add":self.calc("add"),
                font="Helvetica").pack(side = LEFT)
-        Button(self, text="Subtract!", command=self.calc,
+        Button(self, text="Subtract!", command=lambda m="sub":self.calc("sub"),
                font="Helvetica").pack(side = LEFT)
-        Button(self, text="Multiply!", command=self.calc,
+        Button(self, text="Multiply!", command=lambda m="mul":self.calc("mul"),
                font="Helvetica").pack(side = LEFT)
-        Button(self, text="Divide!", command=self.calc,
+        Button(self, text="Divide!", command=lambda m="div":self.calc("div"),
                font="Helvetica").pack(side = LEFT)
            
-    def calc(self):
+    def calc(self, oper):
         try:
             num1 = float(self.num1.get())
             num2 = float(self.num2.get())
@@ -47,56 +47,20 @@ class ControlPanel(Frame):
             return None
 
         self.disp.delete(0, END)
-        self.disp.insert(0, str(num1 + num2))
+
+        if oper == "add":
+            self.disp.insert(0, str(num1 + num2))
+        elif oper == "sub":
+            self.disp.insert(0, str(num1 - num2))
+        elif oper == "mul":
+            self.disp.insert(0, str(num1 * num2))
+        elif oper == "div":
+            self.disp.insert(0, str(num1 / num2))
 
     def reset(self):
         self.num1.delete(0, END)
         self.num2.delete(0, END)
         self.disp.delete(0, END)
-
-class EmptyFrame(Frame):
-    def __init__(self, app):
-        Frame.__init__(self, app)
-
-        Label(self, text='').pack()
-
-class OpLabelFrame(Frame):
-    def __init__(self, app, operation):
-        Frame.__init__(self, app)
-
-        Label(self, text=operation, fg="#FFFFFF",
-              bg="#0066CC").pack(ipadx=5, ipady=3, side=LEFT)
-
-class OperationPanel(Frame):
-    def __init__(self, app, operation):
-        Frame.__init__(self, app)
-
-        EmptyFrame(app).pack(expand = True, fill = X)
-        
-        self.oper = operation[0:3]
-        OpLabelFrame(app, operation).pack(side = TOP, expand = True, fill = X,
-                                          padx = 4)
-        
-        
-        if self.oper == "Add":
-            text_="+"
-        elif self.oper == "Sub":
-            text_="-"
-        elif self.oper == "Mul":
-            text_="*"
-        elif self.oper == "Div":
-            text_="/"
-            
-        Label(self, text=text_, fg="Blue").pack(side = LEFT)
-        
-        Label(self, text="=", fg="Blue").pack(side=LEFT)
-        
-        self.disp = Entry(self, width = 15, font = "Helvetica",
-                          highlightcolor="#0066FF", fg = "#505050")
-        self.disp.pack(side = LEFT, padx = 4)
-        
-        
-
     
 app = Tk()
 app.title("My calculator")
