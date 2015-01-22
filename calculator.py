82b9c9ab03ce3f91472ce5c101c362483d681f9e
Hrudaya Ranjan Sahoo <hrudayranjansahoo0208@gmail.com>
2015-01-22 08:31:03 +0530

    Minor modifications

--- calculator.py
+++ calculator.py
@@ -6,67 +6,30 @@ import pygame.mixer
 class OperationPanel(Frame):
     def __init__(self, app, operation):
         Frame.__init__(self, app)
-        if operation == "addition":
-            self.oper = "add"
-            Label(self, text="Addition:", fg="blue").pack()
-            self.num1 = Entry(self)
-            self.num1.pack(side = LEFT)
-            self.num2 = Entry(self)
-            self.num2.pack(side = LEFT)
-            self.disp = Entry(self)
-            self.disp.pack(side = LEFT)
-            Button(self, text="Reset", command=self.reset).pack(side = RIGHT)
-            Button(self, text="Add!",  command=self.calc).pack(side = RIGHT)
-            
-        elif operation == "subtraction":
-            self.oper = "sub"
-            Label(self, text="Subtraction:", fg="blue").pack()
-            self.num1 = Entry(self)
-            self.num1.pack(side = LEFT)
-            self.num2 = Entry(self)
-            self.num2.pack(side = LEFT)
-            self.disp = Entry(self)
-            self.disp.pack(side = LEFT)
-            Button(self, text="Reset", command=self.reset).pack(side = RIGHT)
-            Button(self, text="Subtract!", command=self.calc).pack(side = RIGHT)
-            
-        elif operation == "multiplication":
-            self.oper = "mul"
-            Label(self, text="Multiplication:", fg="blue").pack()
-            self.num1 = Entry(self)
-            self.num1.pack(side = LEFT)
-            self.num2 = Entry(self)
-            self.num2.pack(side = LEFT)
-            self.disp = Entry(self)
-            self.disp.pack(side = LEFT)
-            Button(self, text="Reset", command=self.reset).pack(side = RIGHT)
-            Button(self, text="Multiply!",  command=self.calc).pack(side = RIGHT)
 
-        elif operation == "division":
-            self.oper = "div"
-            Label(self, text="Division:", fg="blue").pack()
-            self.num1 = Entry(self)
-            self.num1.pack(side = LEFT)
-            self.num2 = Entry(self)
-            self.num2.pack(side = LEFT)
-            self.disp = Entry(self)
-            self.disp.pack(side = LEFT)
-            Button(self, text="Reset", command=self.reset).pack(side = RIGHT)
-            Button(self, text="Divide!",  command=self.calc).pack(side = RIGHT)
-
+        self.oper = operation[0:3]
+        Label(self, text=operation, fg="blue").pack()
+        self.num1 = Entry(self)
+        self.num1.pack(side = LEFT)
+        self.num2 = Entry(self)
+        self.num2.pack(side = LEFT)
+        self.disp = Entry(self)
+        self.disp.pack(side = LEFT)
+        Button(self, text="Reset", command=self.reset).pack(side = RIGHT)
+        Button(self, text=(operation + "!"),  command=self.calc).pack(side = RIGHT)
            
     def calc(self):
         num1 = int(self.num1.get())
         num2 = int(self.num2.get())
         self.disp.delete(0, END)
 
-        if self.oper == "add":
+        if self.oper == "Add":
             self.disp.insert(0, str(num1 + num2))
-        elif self.oper == "sub":
+        elif self.oper == "Sub":
             self.disp.insert(0, str(num1 - num2))
-        elif self.oper == "mul":
+        elif self.oper == "Mul":
             self.disp.insert(0, str(num1 * num2))
-        elif self.oper == "div":
+        elif self.oper == "Div":
             self.disp.insert(0, str(num1 / num2))
 
     def reset(self):
@@ -81,9 +44,9 @@ app.title("My calculator")
 sounds = pygame.mixer
 sounds.init()
 
-OperationPanel(app, "addition").pack(fill=X)
-OperationPanel(app, "subtraction").pack(fill=X)
-OperationPanel(app, "multiplication").pack(fill=X)
-OperationPanel(app, "division").pack(fill=X)
+OperationPanel(app, "Addition").pack(fill=X)
+OperationPanel(app, "Subtraction").pack(fill=X)
+OperationPanel(app, "Multiplication").pack(fill=X)
+OperationPanel(app, "Division").pack(fill=X)
 
 app.mainloop()
