01e4ff0454471ac19b701d1b9b910009fe706b06
Hrudaya Ranjan Sahoo <hrudayranjansahoo0208@gmail.com>
2015-01-22 11:49:23 +0530

    Added color to 'Reset' and 'Operation' buttons and changed their font to 'Helvetica'. Moved the p[eration labels to the leftmost part and the rest of the operation widgets to the bottom of the corresponding labels with the use of new widgets.

--- calculator.py
+++ calculator.py
@@ -3,24 +3,60 @@
 from tkinter import *
 import pygame.mixer
 
+class EmptyFrame(Frame):
+    def __init__(self, app):
+        Frame.__init__(self, app)
+
+        Label(self, text='').pack()
+
+class OpLabelFrame(Frame):
+    def __init__(self, app, operation):
+        Frame.__init__(self, app)
+
+        Label(self, text=operation, fg="#FFFFFF",
+              bg="#0066CC").pack(ipadx=5, ipady=3, side=LEFT)
+
 class OperationPanel(Frame):
     def __init__(self, app, operation):
         Frame.__init__(self, app)
 
+        EmptyFrame(app).pack(expand = True, fill = X)
+        
         self.oper = operation[0:3]
-        Label(self, text=operation, fg="#FFFFFF").pack()
-        self.num1 = Entry(self)
-        self.num1.pack(side = LEFT)
-        self.num2 = Entry(self)
-        self.num2.pack(side = LEFT)
-        self.disp = Entry(self)
-        self.disp.pack(side = LEFT)
-        Button(self, text="Reset", command=self.reset).pack(side = RIGHT)
-        Button(self, text=(operation + "!"),  command=self.calc).pack(side = RIGHT)
+        OpLabelFrame(app, operation).pack(side = TOP, expand = True, fill = X,
+                                          padx = 4)
+        
+        self.num1 = Entry(self, width = 10) 
+        self.num1.pack(padx = 4, side = LEFT)
+        
+        if self.oper == "Add":
+            text_="+"
+        elif self.oper == "Sub":
+            text_="-"
+        elif self.oper == "Mul":
+            text_="*"
+        elif self.oper == "Div":
+            text_="/"
+            
+        Label(self, text=text_, fg="Blue").pack(side = LEFT)
+        
+        self.num2 = Entry(self, width = 10)
+        self.num2.pack(side = LEFT, padx = 4)
+        
+        Label(self, text="=", fg="Blue").pack(side=LEFT)
+        
+        self.disp = Entry(self, width = 15)
+        self.disp.pack(side = LEFT, padx = 4)
+        
+        Button(self, text="Reset", command=self.reset,
+               bg="#A00000", fg="#FFFFFF", relief="raised",
+               font="Helvetica").pack(side = RIGHT)
+        Button(self, text=(operation + "!"),  command=self.calc,
+               font="Helvetica").pack(side = RIGHT)
            
     def calc(self):
-        num1 = int(self.num1.get())
-        num2 = int(self.num2.get())
+        num1 = float(self.num1.get())
+        num2 = float(self.num2.get())
         self.disp.delete(0, END)
 
         if self.oper == "Add":
@@ -44,9 +80,9 @@ app.title("My calculator")
 sounds = pygame.mixer
 sounds.init()
 
-OperationPanel(app, "Addition").pack(fill=X)
-OperationPanel(app, "Subtraction").pack(fill=X)
-OperationPanel(app, "Multiplication").pack(fill=X)
-OperationPanel(app, "Division").pack(fill=X)
+OperationPanel(app, "Addition").pack(fill=X, pady=5)
+OperationPanel(app, "Subtraction").pack(fill=X, pady=10)
+OperationPanel(app, "Multiplication").pack(fill=X, pady=10)
+OperationPanel(app, "Division").pack(fill=X, pady=10)
 
 app.mainloop()
